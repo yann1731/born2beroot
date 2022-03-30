f1394bf5e9aad340e14606bce38778aabca091f0
yan <yan_1731@hotmail.com>
2022-03-30 15:59:16 -0400

    let's go

--- monitoring.sh
+++ monitoring.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-arc = ${uname -a}
-cpu = ${}
 
-wall "
-	#Architecture: $arc
-"
+arc=$(uname -a)
+cpu=$(cat /proc/cpuinfo | grep "cpu cores"| uniq | awk '{print $4}')
+vcpu=$(nproc --all)
+ram=$(free -m | grep "Mem" | awk '{print $2 / 1000}')
+ramusage=$(free -m | grep "Mem" | awk '{print $3 / $2 * 100}')
+
+wall " #Architecture: $arc
+	#Physical CPU cores: $cpu
+	#Virtual CPU cores: $vcpu
+	#Total RAM: $ram GB
+	#RAM usage: $ramusage % "
