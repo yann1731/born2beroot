b79dd8263e258d9ab25a1fa8674e34ef3cecfcec
fenix144 <yan_1731@hotmail.com>
2022-04-06 12:24:26 -0400

    let's goooo

--- monitoring.sh
+++ monitoring.sh
@@ -1,16 +1,15 @@
-#!/bin/bash
-arc=$(uname -a)
+#!/bin/sh
+arc=$(uname -a | awk '{print $3, $4, $5, $6, $7, $9, $10}')
 cpu=$(cat /proc/cpuinfo | grep "cpu cores"| uniq | awk '{print $4}')
 vcpu=$(nproc --all)
 ram=$(free -m | grep "Mem" | awk '{print $2 / 1000}')
 ramusage=$(free -m | grep "Mem" | awk '{print $3 / $2 * 100}')
-storage=$(df -Bg | grep "boot" | awk'{tot += $}')
 fdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
 udisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
 pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
 ucpu=$(mpstat | grep "all" | awk '{print 100 - $13}')
-lastboot=$(who -b | awk '{print $1: $3 $4}')
-sincelb=$(uptime | awk '{print $2}')
+lastboot=$(uptime -s | awk '{print $1, $2}')
+sincelb=$(uptime | awk '{print $3}')
 lvmc=$(lsblk | grep "lvm" | wc -l)
 lvmstat=$(if [ $lvmc -eq 0 ]; then echo no; else echo yes; fi)
 con=$(ss | grep "tcp" | grep "ESTAB" | wc -l)
@@ -25,7 +24,7 @@ wall "
 	#Virtual CPU cores: $vcpu
 	#Total RAM: $ram GB
 	#RAM usage(%): $ramusage%
-	#Storage space: udisk/${fdisk}Gb ($pdisk%)
+	#Storage space(MB/GB)(%): $udisk/${fdisk}B ($pdisk%)
 	#CPU usage(%): $ucpu%
 	#Date of last reboot: $lastboot
 	#Elapsed time since last reboot: $sincelb
@@ -34,5 +33,8 @@ wall "
 	#Users on server: $users
 	#IPV4 adress: $ip
 	#MAC adress: $macaddr
-	#Number of sudo commands used: $sudocmd
-	"
+	#Number of sudo commands used: $sudocmd"
+
+#Crontab
+@reboot	root	sleep 5; bash /root/monitoring.sh
+*/10 * * * *	root	bash /root/monitoring.sh
